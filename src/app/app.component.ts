8a80f3d6e872be9fe6f7c41b742c89dbba5fdc30
Henry Chon 2 <chonhenry@outlook.com>
2022-07-03 15:25:00 -0400

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'basics';
+  name = 'henry chon';
+  imgUrl =
+    'https://i.picsum.photos/id/237/500/500.jpg?hmac=idOEkrJhLd7nEU5pNrAGCyJ6HHJdR_sit1qDt5J3Wo0';
+  images = [
+    'https://i.picsum.photos/id/1003/1181/1772.jpg?hmac=oN9fHMXiqe9Zq2RM6XT-RVZkojgPnECWwyEF1RvvTZk',
+    'https://i.picsum.photos/id/1001/5616/3744.jpg?hmac=38lkvX7tHXmlNbI0HzZbtkJ6_wpWyqvkX4Ty6vYElZE',
+    'https://i.picsum.photos/id/1002/4312/2868.jpg?hmac=5LlLE-NY9oMnmIQp7ms6IfdvSUQOzP_O3DPMWmyNxwo',
+  ];
+  currentDate = new Date();
+  cost = 2000;
+  temperature = 25.3;
+  pizza = {
+    toppings: ['pepperoni', 'bacon'],
+    size: 'large',
+  };
+  blueClass = false;
+  fontSize = 16;
+
+  getName() {
+    return this.name;
+  }
+
+  changeImage(e: KeyboardEvent) {
+    this.imgUrl = (e.target as HTMLInputElement).value;
+  }
+
+  logImg(event: string) {
+    console.log(event);
+  }
 }
